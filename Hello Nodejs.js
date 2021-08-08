7776773cbcac8f9b2c4dc1f31049a947507a423b
LeeWoojin-99 <lwj1316@naver.com>
2021-08-09 04:16:21 +0900

    ndoe.js에 대한 기초적인 설명

---
+++ Hello Nodejs.js
@@ -0,0 +1,6 @@
+// node.js
+// javascript 언어를 웹브라우저가 아닌 컴퓨터에서 동작할 수 있게하는 환경을 node.js라고 한다.
+// 명령어 node를 통해 js파일을 실행시킬 수 있다.
+// node.js가 없다면 javascript는 웹브라우저에서만 쓰일 수 있다.
+
+console.log("Hello Node.js");
