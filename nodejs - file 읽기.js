6be017fe990fdc1fd4c41bd866477d7399e4972d
LeeWoojin-99 <lwj1316@naver.com>
2021-08-12 21:19:51 +0900

    nodejs file 읽기

---
+++ nodejs - file 읽기.js
@@ -0,0 +1,44 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+
+var app = http.createServer(function(request, response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
+
+    if(_url === "/"){
+        title = "Welcome";
+    }
+    if(_url === "/favicon.ico"){
+        return response.writeHead(404);
+    }
+
+    response.writeHead(200);
+    // response.end(fs.readFileSync(__dirname + _url));
+    // fs.readFileSync(x) : x라는 파일을 읽어온다.
+
+    fs.readFile(`nodejs - file 읽기 ${queryData.id}.txt`, "utf8", function(err, description){
+        var template = `
+        <!--  -->
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+            <title>nodejs로 파일 읽기 - ${title}</title>
+        </head>
+        <body>
+            <h1>${title}</h1>
+            <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <p>${description}</p>
+        </body>
+        </html>
+        `
+        response.end(template);
+    })
+})
+app.listen(3000);
