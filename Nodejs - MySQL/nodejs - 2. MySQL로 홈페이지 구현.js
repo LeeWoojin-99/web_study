75d63f7dc00349fb4f4349e07516a438f33d70df
LeeWoojin-99 <lwj1316@naver.com>
2021-09-08 07:52:37 +0900

    nodejs - 2. MySQL로 홈페이지 구현

---
+++ Nodejs - MySQL/nodejs - 2. MySQL로 홈페이지 구현.js
@@ -0,0 +1,172 @@
+// nodejs - 2. MySQL로 홈페이지 구현
+
+// 데이터를 디렉터리의 파일로 관리하던 것을 database로 관리하기 위해서
+// fs모듈을 사용하던 부분을 mysql모듈 사용하도록 변경하였다.
+
+// template module에서 파일 목록을 생성하는 부분을 수정하였음
+// old : template.makeFileList()의 인수값으로 파일 목록이 담긴 arr을 주었음.
+// new : 인수값으로 파일명을 속성으로 가지는 객체가 담긴 arr을 주었음.
+// tempalte module에서 객체에서 속성을 불러와서 파일명을 읽도록 수정하였다.
+
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var qs = require("querystring");
+// html의 form 태그에서 서버로 보내는 데이터가 query string 형태의 데이터이기 때문에 받아온 데이터를 js의 객체 형태로 변환해주는 기능을 사용하기 위한 'querystring' 모듈
+var template = require("./nodejs - template module (MySQL).js"); // html code를 만들어주는 메서드가 담긴 모듈
+var mysql = require("mysql");
+var db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "lwj1316",
+    database: "opentutorials"
+});
+db.connect();
+
+var app = http.createServer(function(request, response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathName = url.parse(_url, true).pathname;
+
+    var title; // 제목
+    var contents; // 내용
+    var list; // 리스트
+
+    if(pathName === "/"){ // home page로 왔을 때
+        if(queryData.id === undefined){ // query string이 없는 url이라면 home page
+            title = "Home Page";
+            contents = "Hello Node.js";
+
+            // file system code
+            // fs.readdir("./nodejs_data", function(err, fileList){
+            //     list = template.makeFileList(fileList); // 읽어온 파일 목록을 ul>li 태그로 구성한 문자열을 반환
+            //     response.writeHead(200); // 정상적인 서버의 응답임을 표시
+            //     response.end(template.html(list, title,
+            //         `<p>${contents}</p>`,
+            //         `<a href="./create">create page</a>`
+            //     )); // title, contents, list, control를 기반으로 html에 대한 template를 작성하여 전송
+            // })
+
+            // MySQL database code
+            db.query(`SELECT title FROM topic`, function(error, titles){
+                if(error) console.log(error);
+                list = template.makeFileList(titles);
+                response.writeHead(200);
+                response.end(template.html(list, title,
+                    `<p>${contents}</p>`,
+                    `<a href="./create">create page</a>`
+                ));
+            });
+        }else{ // query string이 있는 url이라면 해당 페이지를 생성
+            title = queryData.id;
+            fs.readFile(`./nodejs_data/${queryData.id}`, "utf8", function(err, description){
+                contents = description;
+                fs.readdir("./nodejs_data", function(err, fileList){
+                    list = template.makeFileList(fileList);
+                    response.writeHead(200);
+                    response.end(template.html(list, title,
+                        `<p>${contents}</p>`,
+                        `
+                        <a href="./create">create page</a> <a href="./update?id=${title}">update</a>
+                        <form action="/delete_process" method="POST">
+                            <input type="hidden" name="id" value="${title}">
+                            <input type="submit" value="delete">
+                        </form>
+                        `
+                    ));
+                })
+            })
+        }
+    }else if(pathName === "/create"){ // 글 생성 UI 페이지를 만드는 부분
+        title = "Create Page";
+        contents = `
+        <form action="/create_process" method="post">
+            <input type="text" name="title" placeholder="title"><br>
+            <textarea name="description" placeholder="description"></textarea><br>
+            <input type="submit">
+        </form>
+        `;
+        fs.readdir("./nodejs_data", function(err, fileList){
+            list = template.makeFileList(fileList);
+            response.writeHead(200);
+            response.end(template.html(list, title, `<p>${contents}</p>`, ``));
+        })
+    }else if(pathName === "/create_process"){ // 생성된 데이터를 받아서 처리하는 부분
+        var body = "";
+        request.on("data", function(data){
+            body += data;
+        })
+        request.on("end", function(){ // data를 모두 받아왔을 때 실행되는 부분
+            // console.log(body); // query string의 형태로 데이터를 받아왔다.
+            // ex) title=abc&description=a+b+c+d+e
+            var post = qs.parse(body); // qs(query string)의 메서드 parse를 이용하여 query string data를 js object data로 변환한다.
+            // console.log(post); // web browser에서 web server로 보내서 받아진 query string data가 javascript object data로 변환된 데이터이다.
+            title = post.title;
+            description = post.description;
+            fs.writeFile(`nodejs_data/${title}`, description, "utf8", function(err){
+                response.writeHead(302, {Location : `/?id=${title}`});
+                // 서버가 응답을 마치면 Location의 값으로 사용자를 보낸다.
+                // 301 : 페이지가 완전히 이전되었습니다
+                // 302 : 리다이렉션
+                // redirection : 어떠한 처리를 한 후에 사용자를 다른 페이지로 튕겨버리는 것을 리다이렉션이라고 한다.
+                response.end();
+            })
+        })
+    }else if(pathName === "/update"){ // 글 수정 UI 페이지를 만드는 부분
+        title = `${queryData.id}`;
+        fs.readFile(`./nodejs_data/${queryData.id}`, "utf8", function(err, description){
+            contents = `
+            <form action="/update_process" method="post">
+                <input type="hidden" name="id" value="${title}"><br>
+                <input type="text" name="title" placeholder="title" value="${title}"><br>
+                <textarea name="description" placeholder="description">${description}</textarea><br>
+                <input type="submit">
+            </form>
+            `;
+            fs.readdir("./nodejs_data", function(err, fileList){
+                list = template.makeFileList(fileList);
+                response.writeHead(200);
+                response.end(template.html(list, `${queryData.id}`, `<p>${contents}</p>`, ``));
+            })
+        })
+    }else if(pathName === "/update_process"){ // 글 수정 처리를 하는 부분
+        var body = "";
+        request.on("data", function(data){
+            body += data;
+        })
+        request.on("end", function(){
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+
+            fs.rename(`nodejs_data/${id}`, `nodejs_data/${title}`, function(err){
+                // 파일의 이름을 변경
+                fs.writeFile(`nodejs_data/${title}`, description, "utf8", function(err){
+                    // 변경된 이름의 파일의 내용을 변경
+                    response.writeHead(302, {Location: `/?id=${title}`});
+                    // 변경 후에는 해당 위치로 이동
+                    response.end();
+                })
+            })
+        })
+    }else if(pathName === "/delete_process"){ // 삭제 처리 부분
+        var body = "";
+        request.on("data", function(data){
+            body += data;
+        })
+        request.on("end", function(){
+            var post = qs.parse(body);
+            var id = post.id;
+
+            fs.unlink(`nodejs_data/${id}`, function(err){
+                response.writeHead(302, {Location: `/`});
+                response.end();
+            })
+        })
+    }else{ // not found Page
+        response.writeHead(200);
+        response.end("not found");
+    }
+});
+app.listen(3000);
---
+++ Nodejs - MySQL/nodejs - template module (MySQL).js
@@ -0,0 +1,155 @@
+module.exports = {
+    html:function(list, title, body, control){ // HTML에 대한 template를 제작하여 반환하는 함수 함수
+        return `
+            <!--  -->
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+                <title>${title}</title>
+                <style>
+                    /* nodejs application CSS */
+
+                    *{
+                        padding: 0;
+                        margin: 0;
+                    }
+                    html{
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        background-color: rgb(255, 249, 172);
+
+                        font-family: serif;
+                    }
+                    body{
+                        width:600px;
+                    }
+                    a{
+                        text-decoration: none;
+                        color: black;
+                    }
+                    li{
+                        list-style: none;
+                    }
+                    h1{
+                        text-align: center;
+                    }
+                    h1 a{
+                        text-decoration: none;
+                        font-size: 150px;
+                    }
+                    h2{
+                        text-align: center;
+                        font-size: 50px;
+                    }
+                    ul{
+                        margin-bottom: 50px;
+                    }
+                    ul#fileList li a{
+                        display: inline-block;
+                        font-size: 24px;
+                        padding: 3px 12px;
+                        margin: 5px 0;
+                        background: rgb(73, 15, 15);
+                        border-radius: 10px;
+                        color: #fff;
+                        transition: 1s;
+                        font-family: sans-serif;
+                    }
+                    ul#fileList li a:hover{
+                        background: rgb(146, 44, 44);
+                    }
+                    /* create, update, delete */
+                    ul#controlBtnList{
+                        display: flex;
+                    }
+                    .controlBtn{
+                        display: block;
+                        position: relative;
+                        padding: 10px;
+                        margin: 0 20px;
+
+                        text-decoration: none;
+                        font-size: 18px;
+                        font-family: sans-serif;
+                        color: #000;
+
+                        transition: 0.5s;
+                    }
+                    ul:hover .controlBtn{
+                        /* 메뉴에 마우스를 올리면 크기가 커지며 불투명해지고 흐려진다. */
+                        transform: scale(1.5);
+                        opacity: 0.2;
+                        filter: blur(2px);
+                    }
+                    ul:hover .controlBtn:hover{
+                        /* 메뉴 중에서 마우스가 올라간 항목의 크기가 커지며 선명해진다. */
+                        transform: scale(2);
+                        opacity: 1;
+                        filter: blur(0);
+                    }
+                    .controlBtn:before{
+                        /* 마우스가 올라간 항목의 배경에 효과를 주기 위해서 배경을 before로 따로 제작 */
+                        content: "";
+                        position: absolute;
+                        top: 0; left: 0;
+                        width: 100%; height: 100%;
+                        background: #67c92f;
+
+                        transition: 0.5s;
+                        transform-origin: right;
+                        /* 효과가 시작되는 위치를 오른쪽으로 설정 */
+                        transform: scaleX(0);
+                        /* 안보이다가 마우스를 올리면 보이게 할 예정 */
+                        
+                        z-index: -1;
+                        /* 배경을 before을 통하여 따로 제작한 것이기 때문에 a 태그의 뒤로 가도록 z-index를 -1로 설정 */
+                    }
+                    .controlBtn:hover:before{
+                        /* 마우스가 올라가면 크기가 정상으로 */
+                        transform: scaleX(1);
+                    }
+                    form.controlBtn{
+                        padding: 0;
+                    }
+                    .controlBtn input[value="Delete"]{
+                        background-color: transparent;
+                        border: none;
+                        font-size: 18px;
+                        font-family: sans-serif;
+                        color: #000;
+                        cursor: pointer;
+                        padding: 10px;
+                    }
+                    p{
+                        width: 600px;
+                        margin-top: 50px;
+                        padding: 10px;
+                        background: white;
+                        border-radius: 10px;
+                        border: 1px solid black;
+                        font-size: 24px;
+                        font-family: sans-serif;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1><a href="/">WEB</a></h1>
+                <h2>${title}</h1>
+                ${list}
+                ${control}
+                ${body}
+            </body>
+            </html>
+        `;
+    },
+    makeFileList:function(fileList){ // 이름들이 담긴 배열을 인수값으로 받아서 ul태그와 각 이름에 알맞는 li태그들을 구성하는 함수
+        var list = "<ul id=\"fileList\">\n";
+        for(i in fileList){
+            list += `${" ".repeat(4).repeat(5)}<li><a href = "/?id=${fileList[i].title}">${fileList[i].title}</a></li>\n`
+        }
+        list += `${" ".repeat(4).repeat(4)}</ul>\n`;
+        return list
+    }
+}
