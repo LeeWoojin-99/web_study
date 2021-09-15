d2c83df0c418405ed072938475d438bc816f203c
LeeWoojin-99 <lwj1316@naver.com>
2021-09-15 10:39:08 +0900

    들여쓰기 조정

--- nodejs - express/lib/template.js
+++ nodejs - express/lib/template.js
@@ -4,31 +4,30 @@ module.exports = {
     html:function(list, title, body, control){
     // HTML에 대한 template를 제작하여 반환하는 함수 함수
         return `
-            <!--  -->
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>${title}</title>
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                <h2>${sanitizeHtml(title)}</h1>
-                <a href="/author" id="author">author</a>
-                ${list}
-                ${control}
-                ${body}
-            </body>
-            </html>
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>${title}</title>
+        </head>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <h2>${sanitizeHtml(title)}</h1>
+            <a href="/author" id="author">author</a>
+            ${list}
+            ${control}
+            ${body}
+        </body>
+        </html>
         `;
     },
     list:function(fileList){
     // 이름들이 담긴 배열을 인수값으로 받아서 ul태그와 각 이름에 알맞는 li태그들을 구성하는 함수
-        var list = "<ul id=\"fileList\">\n";
+        var list = `<ul id="fileList">\n`;
         for(i in fileList){
             list += `<li><a href = "/page/${fileList[i].id}">${sanitizeHtml(fileList[i].title)}</a></li>\n`
         }
         list += `</ul>\n`;
-        return list
+        return list;
     },
     authorSelect:function(authors, author_id){
     // 저자를 선택하는 select태그, option태그를 구성하는 함수
@@ -43,29 +42,29 @@ module.exports = {
         <select name="author">
         ${optionTag}
         </select>
-        `
+        `;
     },
     authorTable:function(authors){
         var tag = "";
         for (i in authors){
             tag += `
-                <tr>
-                    <td>${sanitizeHtml(authors[i].name)}</td>
-                    <td>${sanitizeHtml(authors[i].profile)}</td>
-                    <td><a href="/author/update/${authors[i].id}">update</a></td>
-                    <td>
-                        <form action="/author/delete_process" method="POST">
-                            <input type="hidden" name="id" value="${authors[i].id}">
-                            <input type="submit" value="Delete" >
-                        </form>
-                    </td>
-                </tr>
+            <tr>
+                <td>${sanitizeHtml(authors[i].name)}</td>
+                <td>${sanitizeHtml(authors[i].profile)}</td>
+                <td><a href="/author/update/${authors[i].id}">update</a></td>
+                <td>
+                    <form action="/author/delete_process" method="POST">
+                        <input type="hidden" name="id" value="${authors[i].id}">
+                        <input type="submit" value="Delete" >
+                    </form>
+                </td>
+            </tr>
             `;
         }
         return `
         <table>
         ${tag}
         </table>
-        `
+        `;
     }
 }
